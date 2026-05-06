d1467d9b015b0044486aae6183611093480ec3a6
Gabrielly Santos <gabriellysantos@live.com>
2026-05-06 19:12:27 -0300

    ✨ feat: adiciona modal ao componente MainHeader

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,18 +1,38 @@
 import { GoPlus, GoCalendar } from "react-icons/go";
 import styles from "./MainHeader.module.css";
+import { useState } from "react";
+import { Modal } from "../Modal/Modal";
+import Cadastro from "../../pages/Cadastro";
 
-export const MainHeader = () => {
+export const MainHeader = ({ onAddTransaction }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
         <div className={styles.container}>
             <h1 className={styles.h1}>Olá, User!</h1>
             <div className={styles.btnWrapped}>
-                <button className={styles.button}>
+                <button
+                    className={styles.button}
+                    onClick={() => setIsModalOpen(true)}
+                >
                     <GoPlus className={styles.iconButton} /> Nova Transação
                 </button>
                 <button className={styles.button}>
                     <GoCalendar className={styles.iconButton} /> Selecionar
                     Período
                 </button>
+
+                <Modal
+                    isOpen={isModalOpen}
+                    onClose={() => setIsModalOpen(false)}
+                >
+                    <Cadastro
+                        onAdd={(novoItem) => {
+                            onAddTransaction(novoItem);
+                            setIsModalOpen(false);
+                        }}
+                    />
+                </Modal>
             </div>
         </div>
     );
