8bbee5db36f0299db06eb4d97adc0d98ec9e8366
Gabrielly Santos <gabriellysantos@live.com>
2026-05-06 19:54:45 -0300

    ♻️ refactor: formata a moeda para BRL usando a funçao formatarMoeda

--- src/components/Resumo/Resumo.jsx
+++ src/components/Resumo/Resumo.jsx
@@ -1,23 +1,24 @@
 import styles from "./Resumo.module.css";
+import { formatarMoeda } from "../../utils/formatadorMoeda";
 
 export const Resumo = ({ receitas, despesas, saldo }) => {
     return (
         <section className={styles.container}>
             <div className={styles.card}>
                 <h3 className={styles.cardTitle}>Receitas</h3>
-                <p className={styles.cardValue}>R$ {receitas}</p>
+                <p className={styles.cardValue}>{formatarMoeda(receitas)}</p>
             </div>
 
             <div className={styles.card}>
                 <h3 className={styles.cardTitle}>Despesas</h3>
                 <p className={`${styles.cardValue} ${styles.cardValueExpense}`}>
-                    R$ {despesas}
+                    {formatarMoeda(despesas)}
                 </p>
             </div>
 
             <div className={styles.card}>
                 <h3 className={styles.cardTitle}>Saldo Atual</h3>
-                <p className={styles.cardValue}>R$ {saldo}</p>
+                <p className={styles.cardValue}>{formatarMoeda(saldo)}</p>
             </div>
 
             <div className={styles.card}>
