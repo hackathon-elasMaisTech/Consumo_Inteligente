c507c64b47d642e81cc82b76b9ffcb2946333367
Gabrielly Santos <gabriellysantos@live.com>
2026-05-07 10:54:15 -0300

    ✨ feat: cria componente ListaTransacoes e CSS Module

--- src/components/ListaTransacoes/ListaTransacoes.jsx
+++ src/components/ListaTransacoes/ListaTransacoes.jsx
@@ -1,24 +1,48 @@
+import styles from "./ListaTransacoes.module.css";
+import { formatarMoeda } from "../../utils/formatadorMoeda";
+import { GoX } from "react-icons/go";
+
 export const ListaTransacoes = ({ consumos, onDelete }) => {
     return (
-        <div>
-            <h2>Lista de Consumos</h2>
+        <div className={styles.container}>
+            <h2 className={styles.titulo}>Lista de Consumos</h2>
 
-            {consumos.length === 0 ? (
+            {consumos.lenght === 0 ? (
                 <p>Nenhum item encontrado</p>
             ) : (
-                consumos.map((item) => (
-                    <div key={item.id}>
-                        <strong>{item.nome}</strong>
-                        {" - R$ "}
-                        {item.valor}
-                        {" - "}
-                        {item.categoria}
-                        {" - "}
-                        {item.tipo}
-
-                        <button onClick={() => onDelete(item.id)}>🗑️</button>
-                    </div>
-                ))
+                <div className={styles.lista}>
+                    {consumos.map((item) => (
+                        <div key={item.id} className={styles.item}>
+                            <div className={styles.infoEsquerda}>
+                                <span className={styles.nome}>
+                                    {item.nome.charAt(0).toLocaleUpperCase() +
+                                        item.nome.slice(1)}
+                                </span>
+                                <span className={styles.categoria}>
+                                    {item.categoria.replace("_", " ")}
+                                </span>
+                            </div>
+                            <div className={styles.infoDireita}>
+                                <span
+                                    className={
+                                        item.tipo === "receita"
+                                            ? styles.valorReceita
+                                            : styles.valorDespesa
+                                    }
+                                >
+                                    {item.tipo === "despesa" ? "- " : "+ "}
+                                    {formatarMoeda(item.valor)}
+                                </span>
+                                <button
+                                    className={styles.btnExcluir}
+                                    onClick={() => onDelete(item.id)}
+                                >
+                                    <GoX />
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             )}
         </div>
     );
