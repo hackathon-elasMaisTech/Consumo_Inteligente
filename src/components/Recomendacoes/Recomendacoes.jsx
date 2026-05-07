5b289e54bbce1a1b7bba28a921ace61a46918e1c
Gabrielly Santos <gabriellysantos@live.com>
2026-05-07 14:07:18 -0300

    ♻️ refactor: converte o componente em uma const com arrow functions

--- src/components/Recomendacoes/Recomendacoes.jsx
+++ src/components/Recomendacoes/Recomendacoes.jsx
@@ -1,39 +1,35 @@
-import styles from "./Recomendacoes.module.css"
+import styles from "./Recomendacoes.module.css";
 import { MdOutlineThumbUp } from "react-icons/md";
 
-
-function Recomendacoes({ analise }) {
-  if (!analise) {
+export const Recomendacoes = ({ analise }) => {
+    if (!analise) {
         return <p>Carregando recomendações...</p>;
-  }
+    }
 
-  const recomendacoes = analise.recomendacoes;
+    const recomendacoes = analise.recomendacoes;
 
-  return (
-          <section className={styles.recomendacoesContainer}>
+    return (
+        <section className={styles.recomendacoesContainer}>
             <h2 className={styles.title}>Recomendações</h2>
 
             {!recomendacoes || recomendacoes.length === 0 ? (
-              <p>Nenhuma recomendação disponível.</p>
-            ): (
-              <div className={styles.recomendacoesList}>
-                {recomendacoes.map((recomendacao) =>(
-                  <article className={styles.recomendacaoItem} key={recomendacao.titulo}>
-                    <span className={styles.icon}>
-                        <MdOutlineThumbUp />
-                    </span>                    
-                    <h4>{recomendacao.titulo}</h4>
-                    <p>{recomendacao.descricao}</p>
-                  </article>
-                
-                
-
-              ))}
-              </div>
-              )}
-          </section>
-  );
-}
-
-
-export default Recomendacoes;
+                <p>Nenhuma recomendação disponível.</p>
+            ) : (
+                <div className={styles.recomendacoesList}>
+                    {recomendacoes.map((recomendacao) => (
+                        <article
+                            className={styles.recomendacaoItem}
+                            key={recomendacao.titulo}
+                        >
+                            <span className={styles.icon}>
+                                <MdOutlineThumbUp />
+                            </span>
+                            <h4>{recomendacao.titulo}</h4>
+                            <p>{recomendacao.descricao}</p>
+                        </article>
+                    ))}
+                </div>
+            )}
+        </section>
+    );
+};
